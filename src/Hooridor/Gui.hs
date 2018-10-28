ad9e70b9ff4e0a90b194fb6524387c0c53e830e2
Luab <lua_b@mail.ru>
2018-10-28 21:28:25 +0300

    Added drawPlayer and drawBoard

--- src/Hooridor/Gui.hs
+++ src/Hooridor/Gui.hs
@@ -17,11 +17,56 @@ background = white
 fps :: Int
 fps = 60
 
-render :: GameState -> Picture
-render _ = blank
+colorPlayer :: PlayerColor -> Color
+colorPlayer Red = red
+colorPlayer Green = green
+colorPlayer Yellow = yellow
+colorPlayer Orange = orange
 
+drawPlayer :: Player -> Picture
+drawPlayer p = translate x y (color (colorPlayer (pcolor p)) (circleSolid 15))
+                where 
+                    (xr,yr) = pos p
+                    x = fromIntegral xr*50
+                    y = fromIntegral yr*50
+
+renderCell :: Cell -> Picture
+renderCell (x,y) = translate (fromIntegral x*50) (fromIntegral y*50) 
+                    ((rectangleSolid 40 40))
+
+drawBoard :: Int -> Picture
+drawBoard m = (pictures 
+            (map renderCell [ (x,y) | x<-[0..m], y<-[0..m]]) ) 
+             
+                
+                
+
+drawWall :: Wall -> Picture
+drawWall ((c1,c2),(c3,c4)) = translate nx1  ny1
+                                    (rectangleSolid 10 10)
+                    where
+                        (x1,y1) = c1 
+                        nx1 = (fromIntegral x1*50-25) 
+                        ny1 = (fromIntegral y1*50-25) 
+
+render :: Int -> GameState -> Picture
+render size state = translate x x (drawBoard size <>
+                pictures (map drawPlayer (playerList state)))
+                    where 
+                        x = fromIntegral (-(size*25))
+                        wall = (((0,1),(1,1)),((1,1),(2,2)))
+
+testCells :: Int -> Int -> [Cell]
+testCells n m = [ ((x,x)) | x <- [0..n] ] 
 update :: Float -> GameState -> GameState
 update _ = id
 
-playGame :: Int -> IO ()
-playGame pc = play window background fps (initialState pc) render handleEvents update
+testState :: GameState 
+testState = initialState 4
+
+testPlayer :: Player
+testPlayer =  Player { pcolor = Red, pos = (0,4), wallsLeft = 5}
+
+playGame :: Int-> Int -> IO ()
+playGame size pc = play window background fps (initialState pc) 
+            (render size) handleEvents update
