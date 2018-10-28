e4af1e31ed0e1a3e591ea20a6dc6bf63424e4fbe
Gheorghe Pinzaru <feross95@gmail.com>
2018-10-28 15:06:08 +0300

    Separate Gui and Core and add init state

--- src/Hooridor.hs
+++ src/Hooridor.hs
@@ -1,16 +1,5 @@
 module Hooridor where
-
-type Cell = (Int, Int)
-
-data PlayerColor = Green | Yellow | Red | Orange
-  deriving (Eq, Enum)
-
-data Player = Player
-  { color     :: PlayerColor
-  , pos       :: Cell
-  , gatesLeft :: Int } deriving (Eq)
-
-data Turn
+import Hooridor.Gui
 
 run :: IO ()
-run = putStrLn "Hello, world!"
+run = playGame 2
