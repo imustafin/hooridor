678b5824d254d359b9c34124542d2bbfa5665879
Gheorghe Pinzaru <feross95@gmail.com>
2018-10-28 18:54:22 +0300

    Finilize wall clipping checks

--- src/Hooridor/Core.hs
+++ src/Hooridor/Core.hs
@@ -13,7 +13,8 @@ data Player = Player
 data Turn = MakeMove Cell
   | PutWall Wall
 
-type Wall = ((Cell, Cell), (Cell, Cell))
+type WallPart = (Cell, Cell)
+type Wall = (WallPart, WallPart)
 data Board = Board [[Cell]]
 
 data GameState = GameState
@@ -27,23 +28,34 @@ cellInBound (x, y) = 0 < x && x < 9 && 0 < y && y < 9
 transposeWall :: Wall -> Wall
 transposeWall ((a, b), (c, d)) = ((a, c), (b, d))
 
+wallPartEq :: WallPart -> WallPart -> Bool
+wallPartEq (a, b) (c, d) = (a == c && b == d) || (a == d && b == c)
+
+wallEq :: Wall -> Wall -> Bool
+wallEq (a, b) (c, d)
+  = (wallPartEq a c && wallPartEq b d)
+  || (wallPartEq a d && wallPartEq b c)
+
+hasWallPart :: WallPart -> Wall -> Bool
+hasWallPart a (b, c) = wallPartEq a b || wallPartEq a c
+
+notInWall :: WallPart -> [Wall] -> Bool
+notInWall step walls' = not (any (hasWallPart step) walls')
+
 tryMove :: Turn -> GameState -> GameState
 tryMove (PutWall wall@((cell1,cell2), (cell3, cell4))) state
   | hasWalls && isInBounds && not intersect = newstate
   | otherwise = state
   where
-    newstate = state {walls = wall : (walls state)
-                     , playerList = others ++ [current {wallsLeft = (wallsLeft current) - 1}]}
+    newstate = state {walls = wall : walls state
+                     , playerList = others ++ [current {wallsLeft = wallsLeft current - 1}]}
     (current:others) = playerList state
     isInBounds = all cellInBound [cell1, cell2, cell3, cell4]
     wall' = transposeWall wall
     coincide (part1, part2)
-      = part1 == (cell1, cell2)
-      || part1 == (cell3, cell4)
-      || part2 == (cell1, cell2)
-      || part2 == (cell3, cell4)
-    hasWalls = (wallsLeft current) > 0
-    intersect = any (\w -> coincide w || w == wall') (walls state)
+      = hasWallPart part1 wall || hasWallPart part2 wall
+    hasWalls = wallsLeft current > 0
+    intersect = any (\w -> coincide w || wallEq w wall') (walls state)
 
 tryMove (MakeMove (x, y)) state
   | cellInBound (x, y) && emptyCell &&
@@ -53,7 +65,9 @@ tryMove (MakeMove (x, y)) state
     newstate = state {playerList = others ++ [current {pos = (x, y)}]}
     (current:others) = playerList state
     emptyCell = all (\p -> (x, y) /= pos p) (playerList state)
-    oneStep (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2) == 1
+    oneStep (x1, y1) (x2, y2)
+      = abs (x1 - x2) + abs (y1 - y2) == 1
+      && notInWall (pos current, (x, y)) (walls state)
     canShortCutTo = any (\o -> oneStep (pos current) (pos o) &&
                                oneStep (pos o) (x, y))
                     others
@@ -65,7 +79,7 @@ initialState :: Int -> GameState
 initialState playerCount = GameState
   { playerList = take playerCount
     [(initPlayer Green (0,4))
-    , (initPlayer Yellow (1,4))
+    , (initPlayer Yellow (8,4))
     , (initPlayer Red (4,0))
     , (initPlayer Orange (4,0))]
   , walls = []
