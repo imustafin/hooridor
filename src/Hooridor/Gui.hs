ce6b513b5de384d8a2be7b78e9fbcac1895a3907
Ilgiz Mustafin <ilgimustafin@gmail.com>
2018-11-23 00:26:06 +0300

    Refactor design

--- src/Hooridor/Gui.hs
+++ src/Hooridor/Gui.hs
@@ -1,5 +1,8 @@
 module Hooridor.Gui where
 
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Data.List
 import Hooridor.Core
   (GameState
   , Cell
@@ -16,56 +19,75 @@ import Hooridor.Core
   , isWinner
   , playerList
   , initialState
+  , minRow
+  , maxRow
+  , minCol
+  , maxCol
+  , size
   )
-import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
-import Data.List
 
 type Board = Cell -> Color
 
-defaultCellColor :: Color
-defaultCellColor = black
+normalCellColor :: Color
+normalCellColor = black
 
 normalWallColor :: Color
-normalWallColor = black
+normalWallColor = red
 
 hintWallColor :: Color
-hintWallColor = greyN 0.5
+hintWallColor = (light . light) normalWallColor
 
 -- |State of the GUI: current GameState and additional decoration to draw
 data GuiState = GuiState GameState Picture
 
-data BoardObject = BoardCell Cell | BoardWall Wall
-  deriving (Show)
+cellSize :: Int
+cellSize = 40
+
+cellMargin :: Int
+cellMargin = 10
+
+cellSizeAndMargin :: Int
+cellSizeAndMargin = cellSize + cellMargin
 
 -- |Center of the Cell drawn on the screen.
 cellCenter :: Cell -> Point
-cellCenter (x,y) = ((fromIntegral (x-4)*50),(fromIntegral (y-4)*50))
+cellCenter (x,y) = ( (fromIntegral (x - halfBoard) * k)
+                   , (fromIntegral (y - halfBoard) * k))
+  where
+    halfBoard = size `div` 2
+    k = fromIntegral cellSizeAndMargin
+
+-- |Board objects that can be pointed at by a mouse
+data BoardObject = BoardCell Cell | BoardWall Wall
+  deriving (Show)
 
 -- |What board object is under the mouse cursor.
 pointingAt :: Point -> Maybe BoardObject
 pointingAt (x, y)
-  | row < 0 || row > 8 || col < 0 || col > 8 = Nothing
+  | row < minRow || row > maxRow || col < minCol || col > maxCol = Nothing
   | not higherThanCell && not righterThanCell
       = Just (BoardCell (row, col))
   | higherThanCell && not righterThanCell
       = Just (BoardWall
                ( ((row, col),     (row + 1, col))
-               , ((row, col + 1), (row + 1, col + 1))))
+               , ((row, col - 1), (row + 1, col - 1))))
   | not higherThanCell && righterThanCell
       = Just (BoardWall
                ( ((row, col),     (row, col + 1))
                , ((row + 1, col), (row + 1, col + 1))))
   | otherwise = Nothing -- Why we need this?
   where
-    y' = round x + 220 :: Int
-    x' = round y + 220 :: Int
-    (xDiv, xMod) = divMod x' 50
-    (yDiv, yMod) = divMod y' 50
+    (minX, minY) = cellCenter (minCol, minRow)
+    shiftX = round (minX - (fromIntegral cellSize / 2))
+    shiftY = round (minY - (fromIntegral cellSize / 2))
+    y' = round x - shiftX
+    x' = round y - shiftY
+    (xDiv, xMod) = divMod x' cellSizeAndMargin
+    (yDiv, yMod) = divMod y' cellSizeAndMargin
     row = yDiv
-    higherThanCell = yMod > 40 -- addrow
+    higherThanCell = yMod > cellSize
     col = xDiv
-    righterThanCell = xMod > 40
+    righterThanCell = xMod > cellSize
 
 -- |Respond to key events.
 handleEvents :: Event -> GuiState -> GuiState
@@ -78,9 +100,9 @@ handleEvents (EventKey (MouseButton _) Down _ (x', y')) gs =
     (GuiState gameState board) = gs
 handleEvents (EventMotion (x',y')) gs
   = case pointingAt (x', y') of
-    Just (BoardCell a) -> GuiState (gameState) (highlightCell a c)
-    Just (BoardWall w) -> GuiState (gameState) (wallHighlighting w)
-    Nothing -> gs
+    Just (BoardCell a) -> GuiState gameState (highlightCell a c)
+    Just (BoardWall w) -> GuiState gameState (wallHighlighting w)
+    Nothing -> GuiState gameState blank
   where
     (GuiState gameState _) = gs
     c = colorPlayer (pcolor (currentPlayer gameState))
@@ -91,7 +113,7 @@ handleEvents (EventKey (Char 'r') _ _ _) _ = initiateGame 2
 handleEvents _ x = x
 
 window :: Display
-window = InWindow "Hooridor? A?" (600, 600) (0, 0)
+window = InWindow "Hooridor" (600, 600) (0, 0)
 
 background :: Color
 background = white
@@ -112,43 +134,47 @@ drawPlayer p = translate x y (color (colorPlayer (pcolor p)) (circleSolid 15))
     (x, y) = cellCenter (xr, yr)
 
 drawCell :: Cell -> Color -> Picture
-drawCell cell c = translate x y ((color c (rectangleSolid 40 40)))
+drawCell cell c = translate x y ((color c (rectangleSolid side side)))
   where
     (x, y) = cellCenter cell
+    side = fromIntegral cellSize
 
 drawDefaultCell :: Cell -> Picture
-drawDefaultCell cell = drawCell cell defaultCellColor
+drawDefaultCell cell = drawCell cell normalCellColor
 
 highlightCell :: Cell -> Color -> Picture
 highlightCell cell c = drawCell cell (dark c)
 
 drawWallSegment :: WallPart -> Color -> Picture
 drawWallSegment wp col
-  | x1 == x2 = translate x y (color col (rectangleSolid 40 5))
-  | otherwise = translate x y (color col (rectangleSolid 5 40))
+  | x1 == x2 = translate x y (color col (rectangleSolid side halfMargin))
+  | otherwise = translate x y (color col (rectangleSolid halfMargin side))
   where
     (c1, c2) = wp
     (x1, _) = cellCenter c1
     (x2, _) = cellCenter c2
     (x, y) = segmentCoordinates wp
+    side = fromIntegral cellSize
+    halfMargin = fromIntegral cellMargin / 2
 
-segmentCoordinates :: WallPart -> (Float,Float)
-segmentCoordinates (c1,c2) = (x,y)
+segmentCoordinates :: WallPart -> (Float, Float)
+segmentCoordinates (c1, c2) = (x, y)
   where
-    (x1,y1) = cellCenter c1
-    (x2,y2) = cellCenter c2
-    x = (x1+x2)/2
-    y = (y1+y2)/2
+    (x1, y1) = cellCenter c1
+    (x2, y2) = cellCenter c2
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
 
 drawWall :: Color  -> Wall  -> Picture
-drawWall c (ws1,ws2) = (drawWallSegment ws1 c)
+drawWall c (ws1, ws2) = (drawWallSegment ws1 c)
   <> (drawWallSegment ws2 c)
-  <> translate x y (color c (rectangleSolid 10 10))
+  <> translate x y (color black (rectangleSolid margin margin))
   where
-    (x1,y1) = segmentCoordinates ws1
-    (x2,y2) = segmentCoordinates ws2
-    x = (x1+x2)/2
-    y = (y1+y2)/2
+    (x1, y1) = segmentCoordinates ws1
+    (x2, y2) = segmentCoordinates ws2
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    margin = fromIntegral cellMargin
 
 drawBoard :: Picture
 drawBoard = pictures cellPictures
